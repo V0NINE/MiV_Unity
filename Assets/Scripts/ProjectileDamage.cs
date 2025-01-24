813bc24093d59c51366e97050aa140da21914fdf
Víctor Fosch Tena <victor.fosch@estudiants.urv.cat>
2025-01-24 17:08:03 +0100

    EntityHealth & ProjectileDamage

---
+++ Assets/Scripts/ProjectileDamage.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class ProjectileDamage : MonoBehaviour
+{
+
+    public int projectileDamage = 1;
+
+    void OnTriggerEnter(Collider other)
+    {
+	if(other.CompareTag("Enemy"))
+	{
+	    EntityHealth entityHealth = other.GetComponent<EntityHealth>();
+
+	    if(entityHealth != null)
+	    {
+		entityHealth.TakeDamage(projectileDamage);
+		Destroy(gameObject);
+		Debug.Log("I shall be destroyed");
+	    }
+	}
+    }
+}
